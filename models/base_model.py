bdfbc293712c4d11001c1643d7153a8166e1ea68
Kufre Akpan <contactkufreakpan@gmail.com>
2024-02-07 16:58:20 +0000

    check if keyword arg are passed

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,20 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    def __init__(self, *args, **kwargs):
+        formatted_time = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, formatted_time))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """
@@ -25,7 +34,6 @@ class BaseModel:
         instn_dict["__class__"] = self.__class__.__name__
         instn_dict["created_at"] = self.created_at.isoformat()
         instn_dict["updated_at"] = self.updated_at.isoformat()
-
         return instn_dict
 
     def __str__(self):
@@ -46,4 +54,13 @@ if __name__ == '__main__':
     print("JSON of my_model:")
     for key in my_model_json.keys():
         print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+		
+    print("--")
+    my_new_model = BaseModel(**my_model_json)
+    print(my_new_model.id)
+    print(my_new_model)
+    print(type(my_new_model.created_at))
+
+    print("--")
+    print(my_model is my_new_model)
 
