915f8935a3f9be5f06fe1d895405a31fc0b43dc7
Kufre Akpan <contactkufreakpan@gmail.com>
2024-02-09 08:31:33 +0000

    added instanced of Basemodel

--- console.py
+++ console.py
@@ -1,28 +1,150 @@
 #!/usr/bin/python
-
+"""
+Module for console
+"""
 import cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     """
+    HBNBCommand console class
     """
     prompt = "(hbnb) "
-
-    def do_quit(self, arg):
-        """
-        """
-        return True
-
-    def help_quit(self, arg):
-        """
-        """
-        print("Quit command to exit the program")
+    valid_classes = ["BaseModel"]
 
     def do_EOF(self, arg):
         """
+        EOF (Ctrl+D) signal to exit the program.
         """
-        print()
         return True
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def do_create(self, arg):
+        """
+        Create a new instance of BaseModel and save it to the JSON file.
+        Usage: create <class_name>
+        """
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            # Assuming BaseModel is the only valid class for now
+            if commands[0] == "BaseModel":
+                new_instance = BaseModel()
+                storage.save()
+                print(new_instance.id)
+
+    def do_show(self, arg):
+        """
+        Show the string representation of an instance.
+        Usage: show <class_name> <id>
+        """
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                print(objects[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Delete an instance based on the class name and id.
+        Usage: destroy <class_name> <id>
+        """
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+            key = "{}.{}".format(commands[0], commands[1])
+            if key in objects:
+                del objects[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Print the string representation of all instances or a specific class.
+        Usage: <User>.all()
+                <User>.show()
+        """
+        objects = storage.all()
+
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            for key, value in objects.items():
+                print(str(value))
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        else:
+            for key, value in objects.items():
+                if key.split('.')[0] == commands[0]:
+                    print(str(value))
+
+    def do_update(self, arg):
+        """
+        Update an instance by adding or updating an attribute.
+        Usage: update <class_name> <id> <attribute_name> "<attribute_value>"
+        """
+        commands = shlex.split(arg)
+
+        if len(commands) == 0:
+            print("** class name missing **")
+        elif commands[0] not in self.valid_classes:
+            print("** class doesn't exist **")
+        elif len(commands) < 2:
+            print("** instance id missing **")
+        else:
+            objects = storage.all()
+
+            key = "{}.{}".format(commands[0], commands[1])
+            if key not in objects:
+                print("** no instance found **")
+            elif len(commands) < 3:
+                print("** attribute name missing **")
+            elif len(commands) < 4:
+                print("** value missing **")
+            else:
+                obj = objects[key]
+                attr_name = commands[2]
+                attr_value = commands[3]
+
+                try:
+                    attr_value = eval(attr_value)
+                except Exception:
+                    pass
+                setattr(obj, attr_name, attr_value)
+
+                obj.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-        
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,47 +2,33 @@ import json
 import os
 from models.base_model import BaseModel
 
+
 class FileStorage:
-    """
-    """
-
     __file_path = "file.json"
-
     __objects = {}
 
-    def new(self, obj):
-        obj_class_name = obj.__class__.__name__
-        key = "{}.{}".format(obj_class_name, obj.id)
-        FileStorage.__objects[key] = obj
-
     def all(self):
-        """
-        """
-        return FileStorage.__objects
+        """Returns the dictionary __objects"""
+        return self.__objects
 
-    def save(self): 
-        """
-        """
-        all_objs = FileStorage.__objects
-        obj_dict = {}
-        for obj in all_objs.keys():
-            obj_dict[obj] = all_objs[obj].to_dict()
-    
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(obj_dict, file, ensure_ascii=False)
-    
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
+
     def reload(self):
-        """
-        """
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-                try:
-                    obj_dict = json.load(file)
-                    for key, value in obj_dict.items():
-                        class_name, obj_id = key.split('.')
-                        cls = eval(class_name)
-                        instance = cls(**value)
-                        FileStorage.__objects[key] = instance
-                except Exception:
-                    pass
-
+        """Deserializes the JSON file to __objects"""
+        try:
+            with open(FileStorage.__file_path, "r") as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    cls_name = value['__class__']
+                    cls = eval(cls_name)
+                    self.__objects[key] = cls(**value)
+        except FileNotFoundError:
+            pass
